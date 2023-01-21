5eb92bddc0fdb6e8cc767ac76534cab23c48e53a
ajayythakur <ajay1270panwar@gmail.com>
2023-01-21 14:53:13 +0530

    React Handson3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [Component, setComponent] = useState(false);
+
+  function backButton() {
+    setComponent((prevValue) => {
+      return !prevValue;
+    });
+  }
+  const [state, setState] = useState({
+    Name: "",
+    Department: "",
+    Rating: "",
+    output: [],
+  });
+
+  let changeEvent = (change) => {
+    setState({ ...state, [change.target.name]: change.target.value });
+    // console.log(this.state);
+  };
+
+  let submitHandler = (sub) => {
+    sub.preventDefault();
+    const empObj = {
+      Name: state.Name,
+      Department: state.Department,
+      Rating: state.Rating,
+    };
+    let arr = state.output;
+    arr.push(empObj);
+    setState({ ...state, output: arr });
+    backButton();
+    document.getElementById("form").reset();
+  };
+  if (Component) {
+    return (
+      <>
+        <button onClick={backButton} id="backButton">
+          GoBack
+        </button>
+        <div id="dataShow">
+          {state.output.map((value, index) => {
+            return (
+              <span id="detailBox" key={index}>
+                Name-{value.Name}
+                <br />
+                Department-{value.Department}
+                <br />
+                Rating-{value.Rating} <br />
+              </span>
+            );
+          })}
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <div id="container">
+        <h1 id="heading">Employee Feedback Form</h1>
+        <form id="form">
+          <label htmlFor="Name">Name</label>
+          <input
+            type="text"
+            id="nameInput"
+            name="Name"
+            value={state.Name}
+            onChange={changeEvent}
+            placeholder="Enter Name"
+          ></input>
+          <br />
+          <label htmlFor="Department">Department</label>
+          <input
+            type="text"
+            id="deptInput"
+            name="Department"
+            value={state.Department}
+            onChange={changeEvent}
+            placeholder="Enter Department"
+          ></input>
+          <br />
+          <label htmlFor="Rating">Rating</label>
+          <select
+            type="number"
+            id="ratingInput"
+            name="Rating"
+            value={state.Rating}
+            onChange={changeEvent}
+            placeholder="Rating"
+          >
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+          <br />
+          <button id="btn" onClick={submitHandler}>
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
